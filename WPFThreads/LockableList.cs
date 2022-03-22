42bc830adc43b2172565be17d9f33c4ec24e3670
vKayroNv <36284611+vKayroNv@users.noreply.github.com>
2022-03-22 16:14:14 +0300

    Создан класс, что бы можно было добавлять и удалять элементы из разных потоков без ошибок

---
+++ WPFThreads/LockableList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFThreads
+{
+    public class LockableList<T>
+    {
+        private List<T> _list = new();
+
+        public void Add(T item)
+        {
+            lock (_list)
+            {
+                _list.Add(item);
+            }
+        }
+
+        public void Remove(T item)
+        {
+            lock (_list)
+            {
+                _list.Remove(item);
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            lock (_list)
+            {
+                _list.RemoveAt(index);
+            }
+        }
+    }
+}
