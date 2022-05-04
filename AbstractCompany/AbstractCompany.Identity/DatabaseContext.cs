a6f8adc21c8a0c61918e7b0244a51cddd62e8064
vKayroNv <36284611+vKayroNv@users.noreply.github.com>
2022-05-04 12:38:06 +0300

    MVC и Identity

---
+++ AbstractCompany/AbstractCompany.Identity/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using AbstractCompany.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbstractCompany.Identity
+{
+    public class DatabaseContext : IdentityDbContext<User>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ AbstractCompany/AbstractCompany.Identity/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AbstractCompany.Identity.Models
+{
+    public class User : IdentityUser
+    {
+        [NotMapped]
+        public override string PhoneNumber { get; set; } = null!;
+        [NotMapped]
+        public override bool PhoneNumberConfirmed { get; set; }
+        [NotMapped]
+        public override bool TwoFactorEnabled { get; set; }
+    }
+}
